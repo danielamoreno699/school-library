06d0e16c0df1909083a80bb04730a9dd62e3ad82
Daniela Moreno <dani9292@hotmail.com>
2023-06-19 12:45:26 -0500

    Add class of student

--- class-person.rb
+++ class-person.rb
@@ -1,34 +1,32 @@
-class Person{
-    def initialize(id, name? = "Unknown", age, parent_permission? = true)
-        @id = id
-        @name = name
-        @age = age
-        
-
-    end
-
-      # accessor get method
-   attr_reader :id, :name, :age
-
-
- # accessor set method
- attr_writer :name, :age
-end
-
-def of_age?(@age)
-    if @age >= 18
-      return true
-    else
-      return false
-    end
-  end
-
-def can_use_services
-    if of_age?(@age) == true || @parent_permission == true
-        return true
-    else
-        return false
+class Person
+    def initialize(id, name = "Unknown", age, parent_permission = true)
+      @id = id
+      @name = name
+      @age = age
+      @parent_permission = parent_permission
     end
   
-
-}
+    # accessor get method
+    attr_reader :id, :name, :age
+  
+    # accessor set method
+    attr_writer :name, :age
+  
+    def of_age?
+      if @age >= 18
+        return true
+      else
+        return false
+      end
+    end
+  
+    #public method
+    def can_use_services
+      if of_age? || @parent_permission
+        return true
+      else
+        return false
+      end
+    end
+  end
+  
