e8926d44bf42147d2a59d2669b45c98d532ae848
Daniela Moreno <dani9292@hotmail.com>
2023-06-20 13:57:04 -0500

    add constructor

--- base_decorator.rb
+++ base_decorator.rb
@@ -1,6 +1,13 @@
 require './nameable'
 
 class BaseDecorator < Nameable
+
+    def initialize(nameable)
+       
+        super(nameable)
+      
+      end
+
   def correct_name?
     @nameable.correct_name?
   end
