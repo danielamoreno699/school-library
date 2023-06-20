7fb7036c4548559ed4dbd2a54cf0d43d31330544
Daniela Moreno <dani9292@hotmail.com>
2023-06-20 14:11:15 -0500

    fix linter

--- base_decorator.rb
+++ base_decorator.rb
@@ -1,12 +1,10 @@
 require './nameable'
 
 class BaseDecorator < Nameable
-
-    def initialize(nameable)
-       
-        super(nameable)
-      
-      end
+  def initialize(nameable)
+    super
+    @nameable = nameable
+  end
 
   def correct_name?
     @nameable.correct_name?
