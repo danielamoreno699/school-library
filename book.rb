08ca0c1f1460f66a0fc137c3bc91c7988366bc51
Daniela Moreno <dani9292@hotmail.com>
2023-06-21 12:58:22 -0500

    Create relationship between book and person using rentals

--- book.rb
+++ book.rb
@@ -1,10 +1,11 @@
 class Book
 
     attr_accessor :title, :author
-    attr_reader :title, :author
+    attr_reader :title, :author, :rentals
 
     def initialize(title, author)
         @title = title
         @author = author
+        @rentals = []
     end
 end
--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ require './nameable'
 class Person < Nameable
   # setters and getters
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
@@ -11,6 +11,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def of_age?
