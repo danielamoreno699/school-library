068cc4ffdafb24f51259848b97ca7ba7df687cdd
Daniela Moreno <dani9292@hotmail.com>
2023-06-21 12:16:19 -0500

    code refactoring

--- base_decorator.rb
+++ base_decorator.rb
@@ -1,10 +1,11 @@
 require './nameable'
 
 class BaseDecorator < Nameable
+  # rubocop:disable Lint/MissingSuper
   def initialize(nameable)
-    super()
     @nameable = nameable
   end
+  # rubocop:enable Lint/MissingSuper
 
   def correct_name?
     @nameable.correct_name?
--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,12 +1,6 @@
 require './base_decorator'
 
 class CapitalizeDecorator < BaseDecorator
-  # rubocop:disable Lint/UselessMethodDefinition
-  def initialize(nameable)
-    super(nameable)
-  end
-  # rubocop:enable Lint/UselessMethodDefinition
-
   def correct_name?
     @nameable.correct_name?.capitalize
   end
