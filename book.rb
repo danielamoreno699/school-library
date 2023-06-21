8a6813924d4b336608f3b4c68113cb21a5cd3daa
Daniela Moreno <dani9292@hotmail.com>
2023-06-21 12:23:13 -0500

    Create book class

---
+++ book.rb
@@ -0,0 +1,12 @@
+class Book
+
+    attr_accessor :title, :author
+
+    attr_reader :title, :author
+
+
+    def initialize(title, author)
+        @title = title
+        @author = author
+    end
+end
