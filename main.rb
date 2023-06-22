3adb490241234c8cfd2ec117f3d7f2fadde478fc
Daniela Moreno <dani9292@hotmail.com>
2023-06-21 20:14:26 -0500

    insert functionality in main rb

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+require_relative 'app'
 
 def main
     puts 'Please select an option:'
@@ -9,4 +10,49 @@ def main
     puts '6. List all rentals for a person'
     puts '7. Quit'
 
+    loop do
+        print 'Enter your choice (1-7): '
+        choice = gets.chomp.to_i
+    
+        case choice
+        when 1
+          app.list_all_books
+        when 2
+          app.list_all_people
+        when 3
+            print 'Enter the person name: '
+            name = gets.chomp
+            print 'Enter the person age: '
+            age = gets.chomp.to_i
+            print 'Enter the person type (student/teacher): '
+            type = gets.chomp.downcase
+            app.create_person(name, age, type)
+        when 4
+            print 'Enter the book title: '
+            title = gets.chomp
+            print 'Enter the book author: '
+            author = gets.chomp
+            app.create_book(title, author)
+        when 5
+            print 'Enter the book title: '
+            book_title = gets.chomp
+            print 'Enter the person name: '
+            person_name = gets.chomp
+            print 'Enter the rental date: '
+            date = gets.chomp
+            app.create_rental(book_title, person_name, date)
+        when 6
+            print 'Enter the person ID: '
+            id = gets.chomp.to_i
+            app.list_rentals_for_person_id(id)
+        when 7
+          puts 'Thank you for using the Library Management System. Goodbye!'
+          break
+        else
+          puts 'Invalid choice. Please try again.'
+        end
+      end
+
 end
+
+main()
