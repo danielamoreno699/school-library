0b55ea7d5d9353ff44fc57fa905fbf98dd79626c
Daniela Moreno <dani9292@hotmail.com>
2023-06-21 20:02:07 -0500

    list all options

---
+++ main.rb
@@ -0,0 +1,12 @@
+
+def main
+    puts 'Please select an option:'
+    puts '1. List all books'
+    puts '2. List all people'
+    puts '3. Create a person'
+    puts '4. Create a book'
+    puts '5. Create a rental'
+    puts '6. List all rentals for a person'
+    puts '7. Quit'
+
+end
